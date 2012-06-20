de13bafea98b1d27c87354fda462baa7c99b388c
sundargates@gmail.com <sundargates@gmail.com@95e073d1-5e0a-81d7-88ca-7190dc2701dd>
2012-06-20 05:16:13 +0000

    Fixes to free unused memory

--- SHA256.js
+++ SHA256.js
@@ -25,8 +25,8 @@ function SHA256(fileSize)
 	
 	function safe_add (x, y) 
 	{
-		var lsw = (x & 0xFFFF) + (y & 0xFFFF);
-		var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+		lsw = (x & 0xFFFF) + (y & 0xFFFF);
+		msw = (x >> 16) + (y >> 16) + (lsw >> 16);
 		return (msw << 16) | (lsw & 0xFFFF);
 	}
  
@@ -41,8 +41,8 @@ function SHA256(fileSize)
  
 	function core_sha256 (m, l, lastPacket) 
 	{
-		var K = new Array(0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3, 0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786, 0xFC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA, 0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147, 0x6CA6351, 0x14292967, 0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13, 0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85, 0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070, 0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3, 0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2);
-		var W = new Array(64);
+		K = new Array(0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3, 0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786, 0xFC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA, 0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147, 0x6CA6351, 0x14292967, 0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13, 0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85, 0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070, 0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3, 0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2);
+		W = new Array(64);
 		var a, b, c, d, e, f, g, h, i, j;
 		var T1, T2;
  
@@ -52,7 +52,7 @@ function SHA256(fileSize)
 			m[((l + 64 >> 9) << 4) + 15] = total_len;
 		}
  
-		for ( var i = 0; i<m.length; i+=16 )
+		for ( i = 0; i<m.length; i+=16 )
 		{
 			a = HASH[0];
 			b = HASH[1];
@@ -63,7 +63,7 @@ function SHA256(fileSize)
 			g = HASH[6];
 			h = HASH[7];
  
-			for ( var j = 0; j<64; j++) {
+			for ( j = 0; j<64; j++) {
 				if (j < 16) W[j] = m[j + i];
 				else W[j] = safe_add(safe_add(safe_add(Gamma1256(W[j - 2]), W[j - 7]), Gamma0256(W[j - 15])), W[j - 16]);
  
@@ -89,21 +89,24 @@ function SHA256(fileSize)
 			HASH[6] = safe_add(g, HASH[6]);
 			HASH[7] = safe_add(h, HASH[7]);
 		}
+
+		delete K, W, a, b, c, d, e ,f, g, h;
+		delete T1, T2;
 	}
  
 	function str2binb (str) {
-		var bin = Array();
-		var mask = (1 << chrsz) - 1;
-		for(var i = 0; i < str.length * chrsz; i += chrsz) {
+		bin = Array();
+		mask = (1 << chrsz) - 1;
+		for(i = 0; i < str.length * chrsz; i += chrsz) {
 			bin[i>>5] |= (str[i / chrsz] & mask) << (24 - i%32);
 		}
 		return bin;
 	}
  
 	function binb2hex (binarray) {
-		var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
-		var str = "";
-		for(var i = 0; i < binarray.length * 4; i++) {
+		hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
+		str = "";
+		for(i = 0; i < binarray.length * 4; i++) {
 			str += hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8+4)) & 0xF) +
 			hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8  )) & 0xF);
 		}
